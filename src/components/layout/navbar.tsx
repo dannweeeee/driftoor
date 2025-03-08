fdb118fe315b903362a0d1f11fbc7e205f7d5fe8
Dann Wee <dannweezj@gmail.com>
2025-03-09 03:06:58 +0800

    fix ui updates

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="fixed top-0 left-0 right-0 isolate z-10 bg-contrast/50 backdrop-filter backdrop-blur-xl py-4"
+        className="fixed top-0 left-0 right-0 isolate z-10 py-4"
       >
         <nav
           aria-label="Desktop navigation"
