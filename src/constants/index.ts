c41151e743a3956c10e00c4d8292d2ebf5c1d743
Dann Wee <dannweezj@gmail.com>
2025-03-14 23:41:16 +0800

    fix: removed redundant files

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-import { Config as CommonConfig } from "@drift-labs/common";
-
-export const SPOT_MARKETS = CommonConfig.spotMarketsLookup;
-
-export const PERP_MARKETS = CommonConfig.perpMarketsLookup;
